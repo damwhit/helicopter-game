978d7f6ed175acbae5afa3b75f0dbc59b0c85574
David Whitaker <damwhit@gmail.com>
2016-05-20 16:07:00 -0600

    collision detection test not working due to jquery and dom

--- test/copter-test.js
+++ test/copter-test.js
@@ -1,5 +1,6 @@
 const expect = require('chai').assert;
 const Copter = require('../lib/copter');
+const Boundary = require('../lib/boundary');
 
 describe('Copter', function() {
   context('with default attributes', function() {
@@ -44,6 +45,20 @@ describe('Copter', function() {
       var copter = new Copter(img, 0, ycoord, 10, 10);
       copter.upLift();
       expect.equal(copter.y, -20);
+    });
+  });
+
+  describe('#checkForCollision', function() {
+    it('should change game status to inactive on collision', function(){
+      var copter = new Copter({}, 0, 198, 10, 10);
+      var boundary = new Boundary(0, 200, 50, 800, 0, { status: "active" }, 0);
+
+      copter.checkForCollision(boundary, 12);
+      expect.equal(boundary.game.status,  'active');
+      copter.gravity();
+
+      copter.checkForCollision(boundary, 12);
+      expect.equal(boundary.game.status, "inactive");
     });
   });
 });
